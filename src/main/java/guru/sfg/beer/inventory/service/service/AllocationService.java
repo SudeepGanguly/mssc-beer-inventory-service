79b33202f80d23d54d8524eace1c63553f12ed63
SSARIpa3 <swati.0491@gmail.com>
2021-05-15 12:00:21 +0530

    Adding Order Allocation Service

---
+++ src/main/java/guru/sfg/beer/inventory/service/service/AllocationService.java
@@ -0,0 +1,7 @@
+package guru.sfg.beer.inventory.service.service;
+
+import guru.sfg.brewery.modal.BeerOrderDto;
+
+public interface AllocationService {
+    Boolean allocateOrder(BeerOrderDto beerOrderDto);
+}
